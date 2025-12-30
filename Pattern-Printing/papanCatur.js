603dbfd14ffbb1200679ac1e4e7b7c7f49a3a741
zegcamegon <zegcamegon@localhost.localdomain>
2025-12-31 04:34:52 +0700

    lanjut besok

--- Pattern-Printing/papanCatur.js
+++ Pattern-Printing/papanCatur.js
@@ -4,6 +4,27 @@
 function papanCatur(num) {
   let pattern = '';
   // code here
+  for (let i = 1; i <= num; i++) {
+    if (i % 2 == 1) {
+      for (let j = 1; j <= num; j++){
+        if (j % 2 == 1) {
+          pattern+="#";
+        } else {
+          pattern+=" ";
+        }
+      }
+    } else {
+      // harusnya gak error ya karena j dijalankan salah satu
+      for (let j = 1; j <= num; j++) {
+        if (j % 2 == 1) {
+          pattern+=" ";
+        } else {
+          pattern+="#";
+        }
+      }
+    }
+    pattern+="\n";
+  }
   return pattern;
 }
 
--- Pattern-Printing/piramidTerbalik.js
+++ Pattern-Printing/piramidTerbalik.js
@@ -1,6 +1,16 @@
 function piramidaTerbalik(num) {
   let pattern = '';
   // code here
+  for (let i = 0; i < num ; i++) {
+    for (let j = 1; j <= num * 2 - i; j++) {
+      if (j - i <= 1) {
+        pattern+=" ";
+      } else {
+        pattern+="*";
+      }
+    }
+    pattern+="\n"
+  }
   return pattern;
 }
 
